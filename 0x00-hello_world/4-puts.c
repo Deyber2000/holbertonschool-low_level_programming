d86e456f98892b8a356dbac14567b22835a4c7f4
Deyber2000 <garzondeiber6@gmail.com>
2020-06-10 21:28:07 +0000

    qw

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-int main(void);
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
+
 }
-
