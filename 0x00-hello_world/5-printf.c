be57c104a585183027de61b13d2ad32ce6332fcd
Deyber2000 <garzondeiber6@gmail.com>
2020-06-10 23:07:50 +0000

    here we go

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-/*Let's to print a message*/
+/**
+ *Let's to print a message
+ *Return: Always 0 (Success)
+ */
 
-int main(void);
+int main(void)
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return(0);
-
+	return (0);
 }
