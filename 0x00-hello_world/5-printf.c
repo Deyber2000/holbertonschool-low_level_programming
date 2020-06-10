feedd391eab068c8c84cb2312dcff3875b38d7e6
Deyber2000 <garzondeiber6@gmail.com>
2020-06-10 22:46:39 +0000

    q

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,9 @@
 
 int main(void);
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	char text[] = "with proper grammar, but the outcome is a piece of art"
+
+
+	printf("%c,\n");
 	return (0);
 }
