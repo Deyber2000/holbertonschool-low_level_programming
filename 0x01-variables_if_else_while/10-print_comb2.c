58e0df7b89cd61bea8f237a50f42fdc39f109036
Deyber2000 <garzondeiber6@gmail.com>
2020-06-12 16:12:21 +0000

    e

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100 ; i++)
+	{
+
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		if (i < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
