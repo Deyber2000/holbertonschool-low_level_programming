3274d71a1a0ea1f669cb845712ad11009eea9a3f
Deyber2000 <garzondeiber6@gmail.com>
2020-06-12 20:14:56 +0000

    w

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	int b;
+
+	int c;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			for (c = b + 1; c <= 9; c++)
+			{
+				putchar(a % 10 + '0');
+				putchar(b % 10 + '0');
+				putchar(c % 10 + '0');
+			if (a != 7)
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
