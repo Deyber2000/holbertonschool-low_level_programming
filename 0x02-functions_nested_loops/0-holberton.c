b9ec1bba82da645d20abcde32bcc08ddce8fe952
Deyber2000 <garzondeiber6@gmail.com>
2020-06-16 13:48:20 +0000

    w

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,7 +4,7 @@
  *main - Entry point
  *Return: Always 0 (Success)
  */
-int _putchar(char c);
+
 
 int main(void)
 {
@@ -17,5 +17,5 @@ int main(void)
 		_putchar(a[i]);
 		i++;
 	}
-	return(0);
+	return (0);
 }
