c2ad8462b913df95a0f82863883c116c23f81835
Deyber2000 <garzondeiber6@gmail.com>
2020-06-16 16:36:02 +0000

    q

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - Entry point
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int letra;
+
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (letra = 'a'; letra <= 'z'; letra++)
+		{
+			_putchar(letra);
+		}
+		_putchar('\n');
+	}
+}
