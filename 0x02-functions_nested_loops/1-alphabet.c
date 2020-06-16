812c273b29dbb9048a1b78e6324ff953edf6e3cf
Deyber2000 <garzondeiber6@gmail.com>
2020-06-16 15:31:38 +0000

    q

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int letra;
+
+	for (letra = 'a'; letra <= 'z'; letra++)
+	{
+		_putchar(letra);
+	}
+	_putchar('\n');
+}
