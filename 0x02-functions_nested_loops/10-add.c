1b9261a59af01c88842cffa66258941b4ea72e69
Deyber2000 <garzondeiber6@gmail.com>
2020-06-17 18:42:11 +0000

    qw

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *add - a function that prints the add of two number
+ * @a: An integer number
+ * @b: Another integer number
+ *Return: returns the result
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
