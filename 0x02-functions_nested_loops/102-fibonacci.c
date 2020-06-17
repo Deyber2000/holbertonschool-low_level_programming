609d3e144f15da48460149de21e220c082edf147
Deyber2000 <garzondeiber6@gmail.com>
2020-06-17 23:03:13 +0000

    qwe

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int f1, f2, i;
+
+	for (i = 1; i <= 50; i++)
+	{
+		printf("%d ", f2);
+		printf(", ");
+		int next = f1 + f2;
+		f1 = f2;
+		f2 = next;
+	}
+	printf("\n");
+	return (0);
+}
