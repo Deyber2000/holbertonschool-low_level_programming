9bd28c2cb2a43b16e32ed86014f9790dcaf29cee
Deyber2000 <garzondeiber6@gmail.com>
2020-06-17 02:17:18 +0000

    w

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,15 @@ int print_last_digit(int n)
 {
 
 	if (n < 0)
+	{
 		n = -1 * (n % 10);
+		_putchar(n + '0');
+		return (n);
+	}
 	else
 	{
 		n = n % 10;
 		_putchar(n + '0');
+		return (n);
 	}
-	return (n);
 }
