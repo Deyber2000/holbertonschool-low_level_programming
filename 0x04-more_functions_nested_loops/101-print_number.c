1002ccb47e02f82bc8ed849707d60f41f80a7448
Deyber2000 <garzondeiber6@gmail.com>
2020-06-20 20:12:54 +0000

    q

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+ * print_number - prints an integer
+ * @n: Integer to print
+ **/
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	putchar(n % 10 + '0');
+}
