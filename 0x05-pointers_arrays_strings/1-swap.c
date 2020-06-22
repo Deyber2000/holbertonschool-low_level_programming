9505d629a5c72b45b55f9b08568a3c65d562c176
Deyber2000 <garzondeiber6@gmail.com>
2020-06-22 23:26:08 +0000

    w

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * swap_int - This function swap the values of the variables through pointers
+ * @a: first pointer
+ * @b: Second pointer
+ **/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+
+	*b = 98;
+}
