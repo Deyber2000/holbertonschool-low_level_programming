f30fc17da2e39e07fba3d56820ac5734d623273b
Deyber2000 <garzondeiber6@gmail.com>
2020-06-24 14:52:23 +0000

    we

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints a string every other
+ * @str: string
+ **/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
