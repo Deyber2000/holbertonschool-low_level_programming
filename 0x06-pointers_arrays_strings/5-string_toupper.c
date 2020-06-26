8ae660046fc45f074d8b54a5293a42055ba13653
Deyber2000 <garzondeiber6@gmail.com>
2020-06-26 00:27:39 +0000

    q

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,15 @@
 #include "holberton.h"
 /**
  * *string_toupper - This function translate from lowescase to uppercase
+ * @s: array with the caracters
  * Return: A char with the change of the letter
  **/
 
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
 	int i;
+
+
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -15,4 +18,5 @@ char *string_toupper(char *)
 			s[i] = s[i] - 32;
 		}
 	}
+	return (s);
 }
