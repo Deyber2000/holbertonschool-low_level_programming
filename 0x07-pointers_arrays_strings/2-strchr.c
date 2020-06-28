84e7e288936c7bfd994f83c64c0bc0b70e1e3538
Deyber2000 <garzondeiber6@gmail.com>
2020-06-28 16:53:46 +0000

    w

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * *_strchr - A function that locates a character in a string
+ * @s: String check
+ * @c: The character that is gonna be searched
+ * Return: A pointer to the first ocurrence in the string
+ **/
+
+char *_strchr(char *s, char c)
+{
+
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (s + 1);
+}
