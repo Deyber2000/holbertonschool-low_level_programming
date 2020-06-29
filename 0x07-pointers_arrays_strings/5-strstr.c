249aa55c925da7f215db02f7d5f32234cbf3ad00
Deyber2000 <garzondeiber6@gmail.com>
2020-06-29 04:17:52 +0000

    Fixing betty

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,8 @@
  * @haystack: String
  * @needle: Substring
  * Return: Returns a pointer to the beginning of the located substring, or NULL
- if the substring is not found.*/
+ * if the substring is not found.
+**/
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int length = 0;
@@ -25,12 +26,14 @@ char *_strstr(char *haystack, char *needle)
  * _strncmp - compares two strings within n bytes.
  * @s1: string to check
  * @s2: string to check from
- * @b: number of bytes to check
+ * @n: number of bytes to check
  * Return: 0 if strings are the same
- * any other number if they are different.*/
+ * any other number if they are different.
+**/
 int _strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i <= n && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
 	{
 		s1++;
