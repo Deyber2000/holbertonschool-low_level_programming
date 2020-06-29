6bac5159d45c6b1e97ebb5391347317e95a37511
Deyber2000 <garzondeiber6@gmail.com>
2020-06-29 18:14:55 +0000

    Fixed 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -18,7 +18,7 @@ void print_diagsums(int *a, int size)
 	{
 		sum1 += a[i];
 	}
-	for (i = (size - 1); i < (size * size); i += (size - 1))
+	for (i = (size - 1); i < (size * size - 1); i += (size - 1))
 	{
 		sum2 += a[i];
 	}
