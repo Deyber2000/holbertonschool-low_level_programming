af57fcad1a09759401aeab34dffa60ca5ec4cf6a
Deyber2000 <garzondeiber6@gmail.com>
2020-06-29 18:39:48 +0000

    Fixed 8

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * set_string -  a function that sets the value of a pointer to a char.
+ * @s: Doble pointer.
+ * @to: Pointer to a char
+ **/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -9,3 +9,4 @@ int _strncmp(char *s1, char *s2, unsigned int n);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
