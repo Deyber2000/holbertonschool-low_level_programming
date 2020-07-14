e3720ab6a21e8d4c0e140e1d5de44f77edd36b85
Deyber2000 <garzondeiber6@gmail.com>
2020-07-14 01:34:48 +0000

    fixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,38 +1,74 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
+char *_strdup(char *str);
+
 /**
- * new_dog - creates a new struct of type dog
- * @name: struct parameter name
- * @age: struct parameter age
- * @owner: struct parameter owner
- * Return: returns pointer to buffer of datatype dog
+ * new_dog - function with 3 arguments
+ * @name: char type pointer
+ * @age: float type
+ * @owner: char type pointer
+ *
+ * Description: creates a new dog
+ * Return: NULL if fail or pointer
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int nlen, olen, i;
-	dog_t *doggy;
+	dog_t *new_d;
 
-	nlen = olen = 0;
-	while (name[nlen++])
-		;
-	while (owner[olen++])
-		;
-	doggy = malloc(sizeof(dog_t));
-	if (doggy == NULL)
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
 		return (NULL);
 
-	doggy->name = malloc(nlen * sizeof(doggy->name));
-	if (doggy == NULL)
+	new_d->name = _strdup(name);
+	if (!new_d->name)
+	{
+		free(new_d);
 		return (NULL);
-	for (i = 0; i < nlen; i++)
-		doggy->name[i] = name[i];
-
-	doggy->age = age;
-
-	doggy->owner = malloc(olen * sizeof(doggy->owner));
-	if (doggy == NULL)
+	}
+	new_d->age = age;
+	new_d->owner = _strdup(owner);
+	if (!new_d->owner)
+	{
+		free(new_d->name);
+		free(new_d);
 		return (NULL);
-	for (i = 0; i < olen; i++)
-		doggy->owner[i] = owner[i];
-	return (doggy);
+	}
+
+	return (new_d);
+
+}
+
+/**
+ * *_strdup - function with one argument
+ * @str: string argument
+ *
+ * Description: returns a pointer to allocated space in memory
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	ptr = malloc(sizeof(char) * i + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		ptr[j] = str[j];
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
 }
