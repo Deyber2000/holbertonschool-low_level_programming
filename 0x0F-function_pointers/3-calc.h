aac5532b3ea2adca97c7bb1cb301d9b3bc57dbb8
Deyber2000 <garzondeiber6@gmail.com>
2020-07-15 18:12:02 +0000

    we

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,10 +1,10 @@
 #ifndef CALC_H
 #define CALC_H
-
 /**
- * struct op - structure of function call
- * @op: operator (+, -, *, /, %)
- * @f: function to call
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
 typedef struct op
 {
@@ -18,5 +18,7 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
 
 #endif
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc,char **argv)
+{
+	int num1, num2;
+	if (argc != 4)
+		printf("Error\n"), exit (98);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (get_op_fun(argv[2]) == NULL)
+		printf("Error\n"), exit(99);
+	if(*argv[2] = '/' && *argv[3] = '0' || *argv[2] = '%' && *argv[3] = '0')
+	{
+		printf("Error\n"), exit(100);
+	}
+	printf("%d\n", (get_op_func(argv[2])(num1, num2)));
+	return (0);
+}
