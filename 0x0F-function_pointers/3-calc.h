8ee0da1a21cf0beb70538b3b745bfcad3fd2089a
Deyber2000 <garzondeiber6@gmail.com>
2020-07-15 18:37:16 +0000

    fixed task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,10 @@
 #ifndef CALC_H
 #define CALC_H
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct op - structure of function call
+ * @op: operator (+, -, *, /, %)
+ * @f: function to call
  */
 typedef struct op
 {
@@ -20,7 +18,5 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-
 
 #endif
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,20 +1,24 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
-
-int main(int argc,char **argv)
+/**
+ * main - define function
+ * @argc: count of arguments
+ * @argv: pointer to array of pointers
+ * Return: 0 on success, 98 on malformed arguments, 99 if no operator
+ */
+int main(int argc, char *argv[])
 {
 	int num1, num2;
+
 	if (argc != 4)
-		printf("Error\n"), exit (98);
+		printf("Error\n"), exit(98);
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if (get_op_fun(argv[2]) == NULL)
+	if (get_op_func(argv[2]) == NULL)
 		printf("Error\n"), exit(99);
-	if(*argv[2] = '/' && *argv[3] = '0' || *argv[2] = '%' && *argv[3] = '0')
-	{
+	if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
 		printf("Error\n"), exit(100);
-	}
 	printf("%d\n", (get_op_func(argv[2])(num1, num2)));
 	return (0);
 }
