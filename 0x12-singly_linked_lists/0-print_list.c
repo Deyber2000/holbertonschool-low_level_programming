6c0a2a4ee5a890e754be997a3c822b89ac94ade0
Deyber2000 <garzondeiber6@gmail.com>
2020-07-23 17:20:46 +0000

    fixed2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,18 @@
 size_t print_list(const list_t *h)
 {
 	size_t j;
-	if (!h)
+	if (h = NULL)
 	{
 		printf("[0] (nil)");
 	}
 
-	for (j = 0; h; j++)
+	else
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		for (j = 0; h; j++)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
 	}
 	return (j);
 }
