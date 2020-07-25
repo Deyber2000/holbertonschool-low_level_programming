b3e166f91f10edc938b922ad27bea578988f743c
Deyber2000 <garzondeiber6@gmail.com>
2020-07-25 23:31:21 +0000

    we

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,25 +1,18 @@
-#ifndef VARIADICFUNCTIONSH
-#define VARIADICFUNCTIONSH
-#include <stdarg.h>
-#include <stdio.h>
+#ifndef VARIADIC_FNS
+#define VARIADIC_FNS
+
+#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+
+typedef struct validTypes
+{
+	char *valid;
+	void (*f)();
+} v_types;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-int _putchar(char c);
-
-/**
- * struct funckey - a key for picking functions to use for printing
- * in 3-print_all.c
- *
- * @f: function to use for printing
- * @spec: character specifier
- */
-typedef struct funckey
-{
-	void (*f)(va_list);
-	char spec;
-} funckey;
 
 #endif
