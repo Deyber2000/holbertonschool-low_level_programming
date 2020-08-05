3b925b264a01a717f56caee2b423c44740109d10
Deyber2000 <garzondeiber6@gmail.com>
2020-08-05 00:24:44 +0000

    fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,8 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 	for (len = 0; text_content[len]; len++)
-		;
+	{
+	}
 	wr_stat = write(new_file, text_content, len);
 	if (close(new_file) == -1)
 		return (-1);
